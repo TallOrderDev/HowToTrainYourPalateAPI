f3bb4d28d3fae6f353fa9b36004c463c8abf7a25
John Gill <j.gill0431@gmail.com>
2016-10-02 17:25:56 -0700

    Finish mock and routes for all but recommendations

--- app/controllers/tried_beer_ratings_controller.rb
+++ app/controllers/tried_beer_ratings_controller.rb
@@ -2,16 +2,18 @@ class TriedBeerRatingsController < ApplicationController
 
   def new
     # Mock
-    user = User.first
     params = 1
     # Real
-    return_types = []
-    main_type BeerType.find(params)
-    taged_types_and_rating_them_return(tagged_type_ids, rating, comment)
+    return_types = [BeerType.find(params)]
+    return_types + (return_types[0].return_sub_types)
   end
 
   def create
-
+    # Mock
+    user = User.first
+    params = {ids: [1,12], comment: "I liked this beer, it was a New Castle", rating: 3}
+    # Real
+    user.taged_types_and_rating_add(params[:ids], params[:rating], params[:comment])
   end
 
 end
