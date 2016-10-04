229ca43ce9eef876dfc3fe814e588f0c2f65e562
Kathryn Ragudos <kragudos19@gmail.com>
2016-10-04 13:45:58 -0700

    Fix more routes

--- app/controllers/tried_beer_ratings_controller.rb
+++ app/controllers/tried_beer_ratings_controller.rb
@@ -3,19 +3,15 @@ class TriedBeerRatingsController < ApplicationController
   before_action :set_user
 
   def new
-    # Mock
-    params = 1
-    # Real
+    params[:beer_type_id]
     return_types = [BeerType.find(params)]
     return_types + (return_types[0].return_sub_types)
   end
 
   def create
-    # Mock
-    user = User.first
-    params = {ids: [1,12], comment: "I liked this beer, it was a New Castle", rating: 3}
-    # Real
-    user.taged_types_and_rating_add(params[:ids], params[:rating], params[:comment])
+    p"^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^"
+    p params
+    # @user.taged_types_and_rating_add(params[:ids], params[:rating], params[:comment])
   end
 
 end
