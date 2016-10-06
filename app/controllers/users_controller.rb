b1162298a3ec13ddc39377eab2162504b3ad2c21
John Gill <j.gill0431@gmail.com>
2016-10-06 10:23:47 -0700

    Add user vote data to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
   before_action :set_user
 
   def show
-    # needs implementation
+    wheel_info = @user.wheel_data_user_data
+    render json:wheel_info
   end
 end
