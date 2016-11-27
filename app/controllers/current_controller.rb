04cff53f925648dbd281a774fb744773ddb85913
TallOrderDev <JohnGill@TallOrderDev.com>
2016-11-26 23:05:02 -0800

    Check for old version app shut down

--- app/controllers/current_controller.rb
+++ app/controllers/current_controller.rb
@@ -1,7 +1,7 @@
 class CurrentController < ApplicationController
 	def show
 		vers = params[:id].to_i
-		if vers >= 1.0 
+		if vers >= 2.0 
 			version_control = true
 		else 
 			version_control = false
